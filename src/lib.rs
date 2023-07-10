590cf2cf800796661e315fb4df05642183c53fd1
Robert Clarke <me@robertclarke.co.uk>
2023-07-10 12:09:38 +0100

    start to implement thread pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
-pub struct ThreadPool;
+use std::fmt;
+use std::error::Error;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Errors
+    ///
+    /// The `build` function will error with [struct@PoolCreationError] if number
+    /// of threads is zero.
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size < 1  {
+            return Err(PoolCreationError::new("Cannot create thread pool of size 0"));
+        }
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        Ok(ThreadPool { threads })
     }
 
     pub fn execute<F>(&self, f: F)
@@ -12,3 +36,23 @@ impl ThreadPool {
         
     }
 }
+
+/// Error while trying to create a new thread pool
+#[derive(Debug)]
+pub struct PoolCreationError {
+    message: String,
+}
+
+impl PoolCreationError {
+    fn new(msg: &str) -> PoolCreationError {
+        PoolCreationError { message: msg.to_string() }
+    }
+}
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for PoolCreationError {}
